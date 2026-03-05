e631546291e2181b32e324ce927448dff6e3a2fb
AnyDev7 jm <anydev@outlook.com>
2026-03-05 16:08:53 -0600

    '*' se agrego a setting.py ALLOWED_HOST(*)

--- kart/settings.py
+++ kart/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = config('SECRET_KEY')  # Se cambio el texto explícito, por el dic d
 # 25 Feb 2026 Antes VPS Hostinger DEBUG = config('DEBUG', default=False, cast=bool)  # Se cambio el texto explícito, por el dic de 'decouple' en '.env'
 DEBUG = False # puesto 25 Feb 2026 para VPS Hostinger
 # 25 Feb 2026 Antes VPS Hostinger ALLOWED_HOSTS = ['ghetti-mart-env.eba-w2yhw2nf.us-west-2.elasticbeanstalk.com', '127.0.0.1']
-ALLOWED_HOSTS = ['anydev7.shop', 'www.anydev7.shop', '191.101.14.94', 'localhost', '127.0.0.1'] # puesto 25 Feb 2026 para VPS Hostinger
+ALLOWED_HOSTS = ['*', 'anydev7.shop', 'www.anydev7.shop', '191.101.14.94', 'localhost', '127.0.0.1'] # puesto 25 Feb 2026 para VPS Hostinger
 
 
 # Application definition
