04ee820b327ac82d3633afbb01af2f0b0529189a
AnyDev7 jm <anydev@outlook.com>
2026-03-21 14:25:13 -0600

    Dar valor inicial a products y ratings, try except

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,11 +4,14 @@ from store.models import Product, Rating
 # Create your views here.
 
 def home(request):
-    
-    products = Product.objects.all().filter(is_available=True).order_by( '-created_at', '-has_discount')
-    for product in products:
-        ratings = Rating.objects.filter(product_id=product.id, status=True)
-
+    products = None
+    ratings = None
+    try:
+        products = Product.objects.all().filter(is_available=True).order_by( '-created_at', '-has_discount')
+        for product in products:
+            ratings = Rating.objects.filter(product_id=product.id, status=True)
+    except:
+        None
     context = {
         'title': "Listado productos",
         'products': products,
